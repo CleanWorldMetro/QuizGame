465eb9468dffa27a2f6adae67ce0539b1f394fdc
viettrung2103 <44894608+viettrung2103@users.noreply.github.com>
2023-11-29 10:57:54 +0200

    implement geo_response

--- back_end/config/dbconfig.py
+++ back_end/config/dbconfig.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+#db connection
 
 connection = mysql.connector.connect(
     host = "127.0.0.1",
@@ -9,3 +10,6 @@ connection = mysql.connector.connect(
     password = "",
     autocommit = True
 )
+
+#openweather key:
+private_key = "28e489100830be62a52cd6f528c12b6c"

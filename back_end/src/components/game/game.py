36b7af90aa0dd3564a23c2532d03701c641a9758
viettrung2103 <44894608+viettrung2103@users.noreply.github.com>
2023-12-10 16:08:26 +0200

    add new current_quiz_session when player_answer

--- back_end/src/components/game/game.py
+++ back_end/src/components/game/game.py
@@ -25,18 +25,15 @@ def get_session_when_player_exist(username):
     # name is exist but no open quiz_session with that name
     if not quiz_session: 
       quiz_sessions.insert_new_quiz_session(username_data[0][0])
-      new_openned_quiz_session = quiz_sessions.get_open_quiz_session_by_player_id(username_data[0][0])
-      quiz_session = new_openned_quiz_session
+      new_opened_quiz_session = quiz_sessions.get_open_quiz_session_by_player_id(username_data[0][0])
+      quiz_session = new_opened_quiz_session
       print("when player exist but quiz_session closed, create new open quiz_session ")
     
     else:
-      print("player exist but quiz_session open, getting that quiz_session")
+      print("player exist and quiz_session open, getting that quiz_session")
     
     return quiz_session
 
-def game() :
-  username = 'hoa'
-  get_quiz_session_by_player(username)
 
 def get_quiz_session_by_player_name(username):
   username_data = players.get_player_by_name(username)
@@ -46,10 +43,56 @@ def get_quiz_session_by_player_name(username):
     quiz_session = get_session_when_player_not_exist(username)
   else:
     quiz_session = get_session_when_player_exist(username)
-  # print(quiz_session)
-      
       
   return quiz_session
+
+def do_insert_new_current_quiz_session(quiz_session_by_player,answer_by_player):
+  print(f'quiz_session: {quiz_session_by_player}')
+  print(f'answer by player: {answer_by_player}')
+  session_id =  quiz_session_by_player[0][0]
+  player_id= quiz_session_by_player[0][1]
+  question_id  = answer_by_player[0][1]
+  question_option_id = answer_by_player[0][0]
+  is_correct = answer_by_player[0][3]
+  current_quiz_sessions.insert_new_current_quiz_session(session_id,player_id,question_id,question_option_id,is_correct)  
+  return
+
+def quiz_session_when_answer_correct(quiz_session_by_player, answer_by_player):
+
+  do_insert_new_current_quiz_session(quiz_session_by_player,answer_by_player)
+  # session_id =  quiz_session_by_player
+  # player_id=
+  # question_id
+  # question_option_id=
+  # is_correct = 
+  return
+
+def quiz_session_when_answer_wrong(quiz_session_by_player, answer_by_player):
+  do_insert_new_current_quiz_session(quiz_session_by_player,answer_by_player)
+
+  
+  return
+
+def save_option_by_option_id(option_id, quiz_session_by_player):
+  answer_option_data = question_options.get_option_by_option_id(option_id)
+  # question_id = 
+  is_correct = question_options.option_is_correct(answer_option_data)
+
+  print(f'answer is correct: {is_correct}')
+  if is_correct:
+    new_quiz_session = quiz_session_when_answer_correct(quiz_session_by_player,answer_option_data)
+  else:
+    new_quiz_session = quiz_session_when_answer_wrong(quiz_session_by_player,answer_option_data)
+  return
+
+def game() :
+  username = 'hoa'
+  quiz_session = get_quiz_session_by_player_name(username)
+  # answer_option_id = request.form.get("option_id")
+  # print(quiz_session)
+  answer_option_id = 19
+  save_option_by_option_id(answer_option_id,quiz_session)
+
 
 if __name__ == "__main__":
   game()
--- back_end/src/components/questions/question_options.py
+++ back_end/src/components/questions/question_options.py
@@ -32,8 +32,17 @@ def get_option_by_option_id(option_id):
     return result
 
 
+def option_is_correct(option):
+    is_correct =  option[0][3]
+    if is_correct == 1:
+        return True
+    else:
+        return False
+    
+
 if __name__ == "__main__":
     options = get_all_options()
     options_by_question = get_options_by_question_id(1)
     option1 = get_option_by_option_id(20)
     print(option1)
+    print(option_is_correct(option1))

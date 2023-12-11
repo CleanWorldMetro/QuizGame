6392f81260cdb6cda9fb94d5661e9e1685989990
viettrung2103 <44894608+viettrung2103@users.noreply.github.com>
2023-12-11 13:40:39 +0200

    finish player-infor page, login page, about page

--- back_end/src/app.py
+++ back_end/src/app.py
@@ -15,8 +15,8 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 
-@app.route("/index")
-@app.route("/home")
+# @app.route("/index")
+# @app.route("/home")
 @app.route("/", methods = ["POST","GET"])
 def index():
   
@@ -31,18 +31,37 @@ def index():
       return redirect(url_for("finish"))
     return render_template("index.html", quiz_session = quiz_session)
   
+@app.route("/home/",methods = ["POST","GET"])
+def home():
+  quiz_session = session.get("quiz_session")
+  
+  if not quiz_session:
+    return redirect(url_for("login"))
+  if request.method == "GET":
+    player = players.get_player_by_player_id(quiz_session[1])[0]
+    # player (1, 'trung')
+    # quiz_session (1, 1, 0, 0, 3, 1)
+    correct_rate = 0
+    if quiz_session[2] > 0: # questions answered more than 0
+      correct_rate = round((quiz_session[3] / quiz_session[2]),2) * 100
+    # print(correct_rate)
+    
+    return render_template("home.html",quiz_session = quiz_session, player = player, correct_rate = correct_rate)
+  else:
+    return redirect(url_for("worldmap"))
+  
 @app.route("/login", methods = ["POST","GET"])
 def login():
   
   if "quiz_session" in session:
-    return redirect(url_for("index"))
+    return redirect(url_for("home"))
   
   if request.method == "POST":
     username = request.form.get("username")
     quiz_session = game.get_quiz_session_by_player_name(username)
     session["quiz_session"] = quiz_session[0]
     flash('Welcome to the Quiz World')
-    return redirect(url_for("index"))
+    return redirect(url_for("home"))
     
   return render_template("login.html")
     
@@ -50,13 +69,18 @@ def login():
 
 @app.route("/worldmap", methods = ["POST","GET"])
 def worldmap():
-  quiz_session = session["quiz_session"]
+  quiz_session = session.get("quiz_session")
+  print(quiz_session)
   if request.method == "GET":
+    if not quiz_session:
+      return redirect(url_for('login'))   
     country_list = countries.get_countries()
     location_list = cities.get_cities()
     
-    correct_points = session["quiz_session"][3]
-    chances = session["quiz_session"][4]
+    # correct_points = session["quiz_session"][3]
+    # chances = session["quiz_session"][4]
+    correct_points = quiz_session[3]
+    chances = quiz_session[4]
     #when victory or game_over
     if correct_points == 10 or chances == 0:
       return redirect(url_for("finish"))
@@ -70,7 +94,8 @@ def worldmap():
 
 @app.route("/location/<city_name>", methods = ["POST","GET"])
 def location(city_name):
-  quiz_session = session["quiz_session"]
+  # quiz_session = session["quiz_session"]
+  quiz_session = session.get("quiz_session")
   if request.method == "GET":
     city_in_request = cities.get_city_by_name(city_name)
     question = questions.get_random_question_by_location_id(city_in_request[0][0])
@@ -81,25 +106,18 @@ def location(city_name):
   else:
     answer_question_option_id = request.form.get("option_id")
     quiz_session_by_player = session['quiz_session']
-    # print(answer_question_option_id) # 12
-    # print(f"current quiz session: {quiz_session_by_player}") # (3, 3, 0, 0, 3, 1)
-    updated_quiz_session = game.save_option_by_option_id(answer_question_option_id,quiz_session_by_player)[0]
 
+    updated_quiz_session = game.save_option_by_option_id(answer_question_option_id,quiz_session_by_player)[0]
+    option =
     #update quiz_session
     session["quiz_session"] = updated_quiz_session
-    
 
-    # print(updated_quiz_session)
-    # answer_option = question_options.get_option_by_option_id(answer_question_option_id)
-    # question_id = answer_option[0][1]
-    # is_correct = answer_option[0][3]
-    #log the answer from user as new current session
-    #up session belong to this user
     return redirect(url_for("worldmap"))
   
 @app.route("/finish", methods = ["POST","GET"])
 def finish():
-  quiz_session = session['quiz_session']
+  # quiz_session = session['quiz_session']
+  quiz_session = session.get("quiz_session")
   print(quiz_session)
   if request.method == "GET":
     correct_counts = quiz_session[3]
@@ -118,10 +136,14 @@ def finish():
     return redirect(url_for("worldmap"))
     
 
+@app.route("/about")
+def about():
+  return render_template("about.html")
+
 @app.route("/logout")
 def logout():
   session.clear()
-  return redirect(url_for("index"))  
+  return redirect(url_for("login"))  
 
 
 
